8551a295aa87d9f13e92bde47057ccdccddd29e2
shammas012 <shammas012@gmail.com>
2025-02-22 01:41:22 +0000

    sample output added
    Sample output added
    Also a bug has been identified during the All suits check. this has been fixed

--- assignments/DeckOfCards.py
+++ assignments/DeckOfCards.py
@@ -39,8 +39,7 @@ for card in cardsJsonObject["cards"]:
     # checking for all same suit
     if(suit == previousSuit):
         suitCounter+=1
-        if(suitCounter==drawCounter): # in this case 5
-            print("Congragulations!!. You draw " + suit + " for all " + drawCounter + " draws")
+        
     elif(drawCounter == 1):
         suitCounter = 1
     else:
@@ -53,14 +52,32 @@ if(len(valueDoubles)>0):
         print(item + "\n")
     # for(value in valueDoubles)
 else:
-    print("No doubles in this draw")
+    print("\nNo doubles in this draw")
 
 if(len(valueTriples)>0):
     print("Here is the list of triples : ")
     for item in valueTriples:
         print(item + "\n")
 else:
-    print("No tripples in this draw")
+    print("No tripples in this draw\n")
+
+if(suitCounter==drawCounter): # in this case 5
+    print("Congragulations!!. You draw " + suit + " for all " + drawCounter + " draws\n")
+else:
+    print("Better luck next time!. Not all suits from the draw are same.\n")
+
+# Sample OutPut :
+# value is 6 and suit is HEARTS
+# value is ACE and suit is HEARTS
+# value is 8 and suit is HEARTS
+# value is ACE and suit is CLUBS
+# value is 3 and suit is HEARTS
+# Here is the list of doubles : 
+# ACE
+
+# No tripples in this draw
+
+# Better luck next time!. Not all suits from the draw are same.
 
 
 
