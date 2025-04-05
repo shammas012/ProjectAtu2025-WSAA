a59e6a796fc012dff174196565355757b7f38b76
shammas012 <shammas012@gmail.com>
2025-04-05 12:01:43 +0100

    Get Absolute path to avoid FileNotFound exception on assignment04

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,7 +1,13 @@
 import requests
+import os
 
 actual_file_name = "sourcefile04.txt"  # name of original file
 replacement_user_name = "shammas"  # name that replaces the word "andrew"
 
-with open(actual_file_name, "r") as file:
-    file = file.read()
+#The application was throwing fileNotFound exception eventhough both of the .py file and .txt file are in the same locations
+#Attempt to get the absolute location of the file
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, actual_file_name)
+
+with open(file_path, "r") as file:
+    fileContent = file.read()
