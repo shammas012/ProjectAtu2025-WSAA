9f41553ec269b169d694556b6930bdf2c836e48b
shammas012 <shammas012@gmail.com>
2025-04-05 12:26:25 +0100

    logic to write file added

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -3,32 +3,32 @@ import os
 
 actual_file_name = "sourcefile04.txt"  # name of original file
 replacement_user_name = "shammas"  # name that replaces the word "andrew"
-new_file_name = "assignment04Output.txt" # output should be saved here
+output_file_name = "assignment04Output.txt" # output should be saved here
 
 #The application was throwing fileNotFound exception eventhough both of the .py file and .txt file are in the same locations
 #Attempt to get the absolute location of the file
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, actual_file_name)
+input_file_path = os.path.join(script_dir, actual_file_name)
 
 #Exception handling for reading file
 try:
     #Readng the file
-    with open(file_path, "r") as file:
+    with open(input_file_path, "r") as file:
         fileContent = file.read()
 except FileNotFoundError:
-    print(f"File not found: {file_path}")
+    print(f"File not found: {input_file_path}")
     exit(1)
 except Exception as e:
-    print(f"Issues with reading file {file_path} : {e}")
+    print(f"Issues with reading file {input_file_path} : {e}")
     exit(1)
 
 try:
-    #Readng the file
-    with open(file_path, "r") as file:
+    #Writing the file
+    with open(output_file_name, "w") as file:
         fileContent = file.read()
 except FileNotFoundError:
-    print(f"File not found: {file_path}")
+    print(f"File not found: {output_file_name}")
     exit(1)
 except Exception as e:
-    print(f"Issues with reading file {file_path} : {e}")
+    print(f"Issues with reading file {output_file_name} : {e}")
     exit(1)
