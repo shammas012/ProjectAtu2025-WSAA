b4c40f0708be3d2934078eafec6e42679a645993
shammas012 <shammas012@gmail.com>
2025-04-05 11:50:21 +0100

    read the file

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,4 +1,7 @@
 import requests
 
-actual_file_name = "sourcefile.txt"  # name of original file
+actual_file_name = "sourcefile04.txt"  # name of original file
 replacement_user_name = "shammas"  # name that replaces the word "andrew"
+
+with open(actual_file_name, "r") as file:
+    file = file.read()
